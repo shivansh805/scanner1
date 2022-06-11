ae911a2a2457dd278434e8eff330d7c8daa127b3
shivansh805 <107304493+shivansh805@users.noreply.github.com>
2022-06-11 17:58:02 +0530

    Add files via upload

---
+++ scanner/src/scanner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class scan {
+    public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+        System.out.print("enter your name:");
+        String s = name.nextLine();
+        System.out.println("hello " +s);
+    }
+
+}
